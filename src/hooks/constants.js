86933799ddd5e839539153ee33f0c3a25c8a9949
nobo <505931977@qq.com>
2017-02-19 12:44:45 +0800

    Modified   src/hooks/constants.js

--- src/hooks/constants.js
+++ src/hooks/constants.js
@@ -19,8 +19,7 @@ export const isUnitlessNumber = {};
 export const skipProps = {};
 export const dehyphenProps = {
     acceptCharset: 'accept-charset',
-    httpEquiv: 'http-equiv',
-    acceptCharset: 'accept-charset'
+    httpEquiv: 'http-equiv'
 };
 export const probablyKebabProps = /^(accentH|arabicF|capH|font[FSVW]|glyph[NO]|horiz[AO]|panose1|renderingI|strikethrough[PT]|underline[PT]|v[AHIM]|vert[AO]|xH|alignmentB|baselineS|clip[PR]|color[IPR]|dominantB|enableB|fill[OR]|flood[COF]|imageR|letterS|lightingC|marker[EMS]|pointerE|shapeR|stop[CO]|stroke[DLMOW]|text[ADR]|unicodeB|wordS|writingM).*/;
 
